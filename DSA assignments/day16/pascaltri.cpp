7aee3368d02e1ba8d07c2abeb96300d2445b902d
shubhamkumar123456 <107460088+shubhamkumar123456@users.noreply.github.com>
2022-07-22 01:14:28 +0530

    Add files via upload

---
+++ DSA assignments/day16/pascaltri.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n,k;
+    cout<<endl<<"enter the limit od pascal triangle :";
+    cin>>n;
+    for(i=1;i<=n;i++){
+        k=1;
+    for(j=1;j<=i;j++){
+        cout<<k<<" ";
+        k=k*(i-j)/j;
+        
+    }
+    cout<<endl;
+    }
+
+}
