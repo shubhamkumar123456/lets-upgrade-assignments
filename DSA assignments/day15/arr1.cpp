2103700cad734b364e389a2df35a049b9e4a24b5
shubhamkumar123456 <107460088+shubhamkumar123456@users.noreply.github.com>
2022-07-21 14:18:11 +0530

    Add files via upload

---
+++ DSA assignments/day15/arr1.cpp
@@ -0,0 +1,15 @@
+// counting even odd positive negative and zero in array of 10 elements
+#include<iostream>
+using namespace std;
+int main(){
+    int a[10],i,p=0,n=0,z=0,e=0,o=0;
+    for(i=0;i<=10-1;i++){
+    cout<<endl<<"enter the elements in array :";
+    cin>>a[i];
+        if()
+
+
+    }
+    
+    
+    }
